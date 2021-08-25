04fcae4720d2f4f812a5a9a53cda5dc20b9ba903
opapinguin <opapinguin@users.noreply.github.com>
2021-08-25 19:22:37 +0200

    Finished command and did some testing
    TODO: 
    - Implement data cleaning function that traverses the string, and throws away login-login or logout-logout events
    - Make the code more readable

--- TimeManagement.cs
+++ TimeManagement.cs
@@ -1,25 +1,25 @@
-﻿/*
+/*
  * Messages players about their time online in the last 24 hours (give or take 30 seconds)
  * Special thanks to Panda and Venk for hosting their plugins in public repos for people to analyze
  */
 
 /* FOR CODERS: Quick note on datapoints in TMTable. A single datapoint is a series of login and logout events. It will look something like:
 * """
-* Player1+ +8/23/2021_6:34:56_PM,-8/23/2021_6:35:04_PM,+8/23/2021_6:37:47_PM,-8/23/2021_6:37:54 PM
-* Player2+ +5/21/2021_2:34:56_PM,-5/21/2021_2:36:14_PM,+5/21/2021_2:38:21_PM,
+* Player1+ +8/23/2021_6:34:56_PM,-8/23/2021_6:35:04_PM,+8/23/2021_6:37:47_PM,-8/23/2021_6:37:54_PM
+* Player2+ +5/21/2021_2:34:56_PM,-5/21/2021_2:36:14_PM,+5/21/2021_2:38:21_PM
+* """
+* The playerTMSettings is a similar text file that tells us which setting to use, and which players to remind. It looks like
+* """
+* Player1+ daily
+* Player2+ current
+* If Player3 has no TM settings he will not be found in this list (thereby keeping it small)
 * """
 */
 
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using MCGalaxy.Modules.Relay.Discord;
 using MCGalaxy.Tasks;
-using MCGalaxy.Events;
 using MCGalaxy.Events.PlayerEvents;
-using MCGalaxy.Commands;
-using MCGalaxy.DB;
 
 namespace MCGalaxy
 {
@@ -30,14 +30,18 @@ namespace MCGalaxy
         public override string MCGalaxy_Version { get { return "1.9.3.4"; } }
 
         public static PlayerExtList TMTable;
+        public static PlayerExtList playerTMSettings;
+
         SchedulerTask task;
-        int HEARTBEAT_TM = 60;
-        int DAY_IN_SECONDS = 86400;
+        const int HEARTBEAT_TM = 60;        // Checks every 60 seconds
+        const int HOUR_IN_SECONDS = 3600;
+        const int DAY_IN_SECONDS = 86400;
 
         public override void Load(bool startup)
         {
             Command.Register(new CmdTimeManagement());
             TMTable = PlayerExtList.Load("extra/timeManagementTable");
+            playerTMSettings = PlayerExtList.Load("extra/playerTMSettings");
             task = Server.MainScheduler.QueueRepeat(TMCallback, null, TimeSpan.FromSeconds(HEARTBEAT_TM));
             OnPlayerConnectEvent.Register(HandlePlayerConnect, Priority.High);
             OnPlayerDisconnectEvent.Register(HandlePlayerDisconnect, Priority.High);
@@ -51,22 +55,15 @@ namespace MCGalaxy
             OnPlayerDisconnectEvent.Unregister(HandlePlayerDisconnect);
         }
 
-        // Repeatedly checks 
         private void TMCallback(SchedulerTask task)
         {
+            // TODO: CLEAN DATA
             DeleteOldTMData();
             MessageOnlinePlayers();
         }
 
-        private void HandlePlayerConnect(Player p)
-        {
-            LogEvent(p, "+");
-        }
-
-        private void HandlePlayerDisconnect(Player p, string reason)
-        {
-            LogEvent(p, "-");
-        }
+        private void HandlePlayerConnect(Player p) { LogEvent(p, "+"); }
+        private void HandlePlayerDisconnect(Player p, string reason) { LogEvent(p, "-"); }
 
         // Adds or alters a data entry to our timeManagementTable.txt. Works by appending the login/logout event
         private void LogEvent(Player p, string symbol)
@@ -116,7 +113,7 @@ namespace MCGalaxy
                 TimeSpan sinceOldestLogout = DateTime.UtcNow - DateTime.Parse(oldestLogout);
 
                 // If the first logout is more than 24 hours ago delete it and the first login (and do this iteratively in case someone relogged several times around that time)
-                while (sinceOldestLogout.TotalSeconds > 60 && timeStamps.Count >= 2)
+                while (sinceOldestLogout.TotalSeconds > DAY_IN_SECONDS && timeStamps.Count >= 2)
                 {
                     timeStamps.RemoveAt(0);
                     timeStamps.RemoveAt(0);
@@ -135,77 +132,67 @@ namespace MCGalaxy
             TMTable.Save();
         }
 
-        private int GetTotalTimeSpent(string pname)
+        private int GetTotalTimeSpent(string pname, string mode)
         {
-            string data = TMTable.FindData(pname);
-
-            DateTime dayAgo = DateTime.UtcNow.AddSeconds(-DAY_IN_SECONDS);
-
-            List<string> timeStamps = new List<string>(data.Split(','));
-            string oldestLogin = timeStamps[0].Substring(1, timeStamps[0].Length - 1).Replace('_', ' ');
-            TimeSpan sinceOldestLogin = DateTime.UtcNow - DateTime.Parse(oldestLogin);
-
-            // Edge case - oldest login is older than a day. Pretend it's exactly a day old
-            if (sinceOldestLogin.TotalSeconds > DAY_IN_SECONDS)
-            {
-                timeStamps[0] = '+' + dayAgo.ToString().Replace(' ', '_');
-            }
-
-            // Edge case - Last entry is a login event. Pretend there's a logout event at the end that's the current date
-            if (timeStamps[timeStamps.Count - 1][0] == '+')
-            {
-                string newestLogout = "-" + DateTime.UtcNow.ToString().Replace(' ', '_');
-                timeStamps.Add(newestLogout);
-            }
-
             int timeSpent = 0;
 
-            for (int i = 0; i < timeStamps.Count; i += 2)
-            {
-                string login = timeStamps[i].Substring(1, timeStamps[i].Length - 1).Replace('_', ' ');
-                string logout = timeStamps[i + 1].Substring(1, timeStamps[i + 1].Length - 1).Replace('_', ' ');
-                DateTime loginDaytime = DateTime.Parse(login);
-                DateTime logoutDaytime = DateTime.Parse(logout);
-                TimeSpan timeOnline = logoutDaytime - loginDaytime;
-                timeSpent += (int)timeOnline.TotalSeconds;
-            }
+            string data = TMTable.FindData(pname);
+            List<string> timeStamps = new List<string>(data.Split(','));
 
+            if (mode == "current")
+            {
+                string loginDate = timeStamps[timeStamps.Count - 1];
+                if (loginDate[0] == '+')
+                {
+                    string login = loginDate.Substring(1, loginDate.Length - 1).Replace('_', ' ');
+                    TimeSpan sinceLogin = DateTime.UtcNow - DateTime.Parse(login);
+                    timeSpent = (int)sinceLogin.TotalSeconds;
+                }
+            }
+            else if (mode == "daily")
+            {
+                DateTime dayAgo = DateTime.UtcNow.AddSeconds(-DAY_IN_SECONDS);
+
+                string oldestLogin = timeStamps[0].Substring(1, timeStamps[0].Length - 1).Replace('_', ' ');
+                TimeSpan sinceOldestLogin = DateTime.UtcNow - DateTime.Parse(oldestLogin);
+
+                // Edge case - oldest login is older than a day. Pretend it's exactly a day old
+                if (sinceOldestLogin.TotalSeconds > DAY_IN_SECONDS)
+                {
+                    timeStamps[0] = '+' + dayAgo.ToString().Replace(' ', '_');
+                }
+
+                // Edge case - Last entry is a login event (it should be of course). Pretend there's a logout event at the end that's the current date
+                if (timeStamps[timeStamps.Count - 1][0] == '+')
+                {
+                    string newestLogout = "-" + DateTime.UtcNow.ToString().Replace(' ', '_');
+                    timeStamps.Add(newestLogout);
+                }
+
+                timeSpent = 0;
+
+                for (int i = 0; i < timeStamps.Count; i += 2)
+                {
+                    string login = timeStamps[i].Substring(1, timeStamps[i].Length - 1).Replace('_', ' ');
+                    string logout = timeStamps[i + 1].Substring(1, timeStamps[i + 1].Length - 1).Replace('_', ' ');
+                    DateTime loginDaytime = DateTime.Parse(login);
+                    DateTime logoutDaytime = DateTime.Parse(logout);
+                    TimeSpan timeOnline = logoutDaytime - loginDaytime;
+                    timeSpent += (int)timeOnline.TotalSeconds;
+                }
+            }
             return timeSpent;
         }
 
-        private bool IsValidTimestamps(List<string> timeStamps)
-        {
-            int stampCount = timeStamps.Count;
-            // Return true if it's just a single login event. Return false if we start with a logout event
-            if (stampCount == 1 && timeStamps[0][0] == '+')
-            {
-                return true;
-            }
-
-            if (timeStamps[0][0] == '-')
-            {
-                return false;
-            }
-
-            // Return false if stamps[i] == stamps[i + 1] (e.g. login followed by login)
-            for (int i = 0; i < stampCount - 1; i++)
-            {
-                if (timeStamps[i][0] == timeStamps[i + 1][0])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private List<string> IterativelyCleanTimeStamps(List<string> stamps, string pName)
+        private List<string> CleanTimeStamps(List<string> stamps, string pName)
         {
             // Get rid of all the useless logout timestamps at the start (although this is really just an extra check, in theory there shouldn't be any)
             while (stamps.Count != 0 && stamps[0][0] == '-')
             {
                 stamps.RemoveAt(0);
             }
+
+            // TODO: MAKE THE BELOW A LOOP THAT TRAVERSES THE STRING
 
             // Update if the first two entries are both logins or logouts (that would be strange indeed)
             if (stamps.Count >= 2 && stamps[0][0] == stamps[1][0])
@@ -221,9 +208,19 @@ namespace MCGalaxy
             Player[] players = PlayerInfo.Online.Items;
             foreach (Player p in players)
             {
-                int timeSpent = GetTotalTimeSpent(p.name);
+                string mode = playerTMSettings.FindData(p.name);
+                if (mode == null) { continue; }
+
+                int timeSpent = GetTotalTimeSpent(p.name, mode);
                 string timeSpentMessage = TMMessages(timeSpent);
-                if (timeSpentMessage != "")
+
+                if (timeSpentMessage == "") { return; }
+
+                if (mode == "current")
+                {
+                    p.Message(TMMessages(timeSpent) + " since you logged in.");
+                }
+                else if (mode == "daily")
                 {
                     p.Message(TMMessages(timeSpent) + " in the last 24 hours.");
                 }
@@ -238,8 +235,11 @@ namespace MCGalaxy
             */
             double X = (seconds + HEARTBEAT_TM / 2) / HEARTBEAT_TM;
             int magicConstant = (int)(Math.Floor(X)) * HEARTBEAT_TM;
+
             switch (magicConstant)
             {
+                case 300:
+                    return "You have been online for 5 minutes";
                 case 600:
                     return "You have been online for 10 minutes";
                 case 1200:
@@ -261,7 +261,15 @@ namespace MCGalaxy
                 case 21600:
                     return "Your time on this planet is short, so take a break, as you have been online for six hours";
                 default:
-                    return "";
+                    if (magicConstant % HOUR_IN_SECONDS == 0)
+                    {
+                        string hours = (magicConstant / HOUR_IN_SECONDS).ToString();
+                        return String.Format("Go outside! You have been online for {0} hours", hours);
+                    }
+                    else
+                    {
+                        return "";
+                    }
             }
         }
     }
@@ -276,19 +284,35 @@ namespace MCGalaxy
         public override void Use(Player p, string message)
         {
             string[] args = message.SplitSpaces();
-            if (args.Length != 1 || (args[0] != "daily" && args[0] != "current")) { Help(p); return; }  // Force people to write "/tm current" OR "/tm daily"
+            if (args.Length != 1) { Help(p); return; }
+            args[0] = args[0].ToLowerInvariant();
 
-
-
-
-
+            switch (args[0])
+            {
+                case "none":
+                    TimeManagement.playerTMSettings.Remove(p.name);
+                    p.Message("TM mode set to none (no notifications)");
+                    break;
+                case "daily":
+                    TimeManagement.playerTMSettings.Update(p.name, "daily");
+                    p.Message("TM mode set to daily");
+                    break;
+                case "current":
+                    TimeManagement.playerTMSettings.Update(p.name, "current");
+                    p.Message("TM mode set to current");
+                    break;
+                default:
+                    Help(p);
+                    break;
+            }
         }
 
         public override void Help(Player p)
         {
-            p.Message("%T/TimeManagement [daily/current]%H- Toggles time management mode");
+            p.Message("%T/TimeManagement [daily/current/none]%H- Toggles time management mode");
             p.Message("Daily mode keeps you up to date on your activity in the last 24 hours.");
             p.Message("Current mode keeps you up to date on your activity since you logged in.");
+            p.Message("Type '/TimeManagement none' to reset to none.");
         }
     }
 }
